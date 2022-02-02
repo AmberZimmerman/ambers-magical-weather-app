a57564f5223a26703f4e9e664f234d2e02d7866a
Amber Zimmerman <ajz1221@gmail.com>
2022-02-01 20:56:50 -0700

    getting forcast temperature

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 
 
 function getApi(cityValue) {
-    // fetch request gets a list of all the repos for the node.js organization
+    // fetch request gets long and lat information for the city that the user types into the input
     var requestUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityValue},1&appid=e411420d201b5d2ab3ba0e99dfa72c08`;
 
     fetch(requestUrl)
@@ -16,6 +16,7 @@ function getApi(cityValue) {
     const lon = data[0].lon;
        console.log(lat, lon);
 
+     // fetch request takes the long and lat defined above and then inserts taht into this web api that gets the forcast information for that location
     let getWeather = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=e411420d201b5d2ab3ba0e99dfa72c08`;
 
     console.log(getWeather);
@@ -34,6 +35,15 @@ function getApi(cityValue) {
         console.log(wind);
         const uvi = weatherData.current.uvi;
         console.log(uvi);
+        const daily = weatherData.daily;
+        console.log(daily);
+
+        for (let i = 0; i < daily.length; i++) {
+            const element = daily[i];
+            const tempDaily = element.temp.min;
+            console.log(tempDaily);
+            console.log(element);
+        }
     })
 
     });
@@ -41,7 +51,8 @@ function getApi(cityValue) {
 }
 
 const city = document.getElementById('city');
-    const submitCity = document.getElementById('submitCity');
+const submitCity = document.getElementById('submitCity');
+
     
     submitCity.addEventListener("click", function(event) {
         event.preventDefault();
