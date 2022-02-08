a63b0d330c832979247cf89ba7454f20d3561c1e
Amber Zimmerman <ajz1221@gmail.com>
2022-02-08 15:46:40 -0700

    adding css sheet and some styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 
+var savedCitiesContainer = document.getElementById('saved-cities');
 
 
         
     
-
         function getApi(cityValue) {
             // fetch request gets long and lat information for the city that the user types into the input
             var requestUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityValue},1&appid=e411420d201b5d2ab3ba0e99dfa72c08`;
@@ -19,7 +19,7 @@
                 console.log(lat, lon);
 
             // fetch request takes the long and lat defined above and then inserts taht into this web api that gets the forcast information for that location
-            let getWeather = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=e411420d201b5d2ab3ba0e99dfa72c08`;
+            let getWeather = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=e411420d201b5d2ab3ba0e99dfa72c08`;
 
             console.log(getWeather);
 
@@ -29,8 +29,11 @@
             })
             .then(function (weatherData) {
                 console.log(weatherData);
+                
+                
 
                 const cardBody = document.getElementById('card-body-current');
+                clearCards(cardBody);
 
                 const date = weatherData.current.dt;
                 const dateEl = document.createElement('div');
@@ -67,6 +70,9 @@
                 const daily = weatherData.daily;
                 console.log(daily);
 
+                const dailyCardsContainer = document.getElementById("daily-cards-container");
+                clearCards(dailyCardsContainer);
+
                 for (let i = 0; i < 5; i++) {
                     const element = daily[i];
 
@@ -74,10 +80,10 @@
                     const tempDaily = element.temp.min;
                     const humidDaily = element.humidity;
                     const windDaily = element.wind_speed;
-                    const uviDaily = element.uvi;
+                    
+                    // const iconDaily = element.weather.
 
-
-                    const dailyCardsContainer = document.getElementById("daily-cards-container");
+                   
 
                     const colDaily = document.createElement('div');
                     colDaily.classList.add('col');
@@ -104,22 +110,9 @@
                     dailyCardBody.appendChild(windDailyEl);
 
                     let humidDailyEl = document.createElement('div');
-                    humidDailyEl.innerText = `Humid: ${humidDaily}`;
-                    dailyCardBody.appendChild(windDailyEl);
-
+                    humidDailyEl.innerText = `Humidity: ${humidDaily}`;
+                    dailyCardBody.appendChild(humidDailyEl);
                     
-
-
-                    
-                    
-
-                    
-            
-            
-                    console.log("temp", tempDaily);
-                    console.log("wind", windDaily);
-                    console.log('humid', humidDaily);
-                    console.log("uvi", uviDaily);
 
                     console.log(element);
                 }
@@ -136,8 +129,9 @@ const submitCity = document.getElementById('submitCity');
 
 submitCity.addEventListener("click", function(event) {
     event.preventDefault();
-    const cityValue = city.value;
-    console.log(cityValue);
+    const cityValue = city.value.trim();
+    if (cityValue) {
+    
     // Get api is strictly putting elements on the page
     getApi(cityValue);
     // local storage is doing its own thing
@@ -151,13 +145,54 @@ submitCity.addEventListener("click", function(event) {
 
     cityNameOne.push(cityValue);
 
-    console.log("cityNameOne", cityNameOne);
+  
     localStorage.setItem("cityNames", JSON.stringify (cityNameOne));
-    
-    console.log('citynamesarray', cityNameOne);
-});
+}
     
 
+    renderHistory();
+  
+
+    
+});
+    
+function renderHistory () {
+    cityNameOne = JSON.parse(localStorage.getItem("cityNames"));
+    if (cityNameOne) {
+    clearCards(savedCitiesContainer);
+
+    
+    for (let index = 0; index < cityNameOne.length; index++) {
+        const cityNamesAgain = cityNameOne[index];
+        console.log(cityNamesAgain);
+
+        const savedCity = document.createElement('button');
+        savedCity.setAttribute("city-name", cityNamesAgain);
+        savedCity.innerText = cityNamesAgain;
+        savedCitiesContainer.append(savedCity);   
+    }
+}
+}
+
+renderHistory();
+
+
+
+function getHistoryAgain (e) {
+    const historyAgainCard = e.target;
+    const storeWord = historyAgainCard.getAttribute("city-name");
+    getApi(storeWord);
+
+}
+
+savedCitiesContainer.addEventListener('click', getHistoryAgain);
+
+function clearCards(x) {
+    while(x.firstChild) {
+x.removeChild(x.firstChild);
+
+    }
+};
 
 
 
@@ -180,4 +215,8 @@ submitCity.addEventListener("click", function(event) {
  * each button will have an event listener
  * each time the button is clicked
  * add event listener to pass in the value again
+ * I'm going to need to check if my button is a button because it will grab any other text floating around
+ * add lat and long not just city name
+ * 
+ trim, make sure it's not an empty string, make sure it's not null
  */
